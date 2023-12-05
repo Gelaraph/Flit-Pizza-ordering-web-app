26705d5eefec1036714d0631a9c3a869ade014be
Gelaraph <angelaraph12@gmail.com>
2023-12-05 16:05:20 +0100

    bug fix

--- src/app/component/PaymentModal.js
+++ src/app/component/PaymentModal.js
@@ -11,16 +11,4 @@ const PaymentModal = ({ closeModal }) => {
   );
 };
 
-{
-  /* <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 py-10 z-50">
-      <div className=" w-full max-w-xl overflow-y-auto sm:rounded-2xl bg-white">
-        <div className="w-full">
-          <div className="m-4 my-5 max-w-[500px] mx-auto">
-            <Form closeModal={closeModal} />
-          </div>
-        </div>
-      </div>
-    </div> */
-}
-
 export default PaymentModal;
